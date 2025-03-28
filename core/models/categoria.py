49d49a39f342d64aaa866dd011ad2d4eed5c3e63
lucas <lucasdesouzasouza57@gmail.com>
2025-03-28 08:28:48 -0300

    Descrição Categoria

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100, verbose_name='Descrição', help_text='Descrição da categoria')
+
+    def __str__(self):
+        return f"({self.id}) {self.descricao}"
